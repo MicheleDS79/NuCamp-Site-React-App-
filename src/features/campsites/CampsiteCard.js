9b2e7137b9b3e766d9882896b9ad8a3e331a2621
Mike Sherokee <michael.sherokee@gmail.com>
2024-04-13 13:46:37 -0400

    Restored Part Image

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 const CampsiteCard = ({campsite}) => {
     return (
         <Card>
-            {/* <CardImg width='100%' src={props.campsite.image} alt={props.campsite.name} /> */}
+            <CardImg width='100%' src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
 
                 <CardTitle>{campsite.name}</CardTitle>
